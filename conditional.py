daa64bc92984e3d6113013e24099fe5e7d9801cd
vinayakck2002 <vinayakck317@gmail.com>
2024-10-04 09:07:22 +0530

    conditional

---
+++ conditional.py
@@ -0,0 +1,12 @@
+a=6
+b=4
+if (a==b):
+    print("they are same")
+else:
+    print("they are not same")
+
+
+    
+    
+    
+    
---
+++ work1.py
@@ -0,0 +1,7 @@
+a=int(input("enter your first num:"))
+b=int(input("enter your second num:"))
+if (a==b):
+    print("true")
+else:
+    print("false")
+    
