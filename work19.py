503971a1e89913ccff16d64d0ce22a101301ade6
vinayakck2002 <vinayakck317@gmail.com>
2024-10-16 09:08:58 +0530

    list

---
+++ work19.py
@@ -0,0 +1,49 @@
+#LIST
+
+# l=[26,25,27,25,27]
+# print(l)
+# print(l[0])
+# print(l[1])
+# print(l[2])
+
+
+#APPEND
+# l.append(28)
+# print(l)
+
+# l.append([100.101])
+# print(l)
+
+#EXTEND
+# l.extend([75,100])
+# print(l)
+
+
+#INSERT
+# l.insert(1,125)
+# print(l)
+# l.insert(2,126)
+# print(l)
+
+#POP
+# l.pop()
+# print(l)
+# l.pop(1)
+# print(l)
+
+
+# list=["apple","orange","grapes","apple"]
+# list.remove("apple")
+# print(list)
+
+# USING FOR LOOP
+# for i in list:
+#     print(i)
+
+# print(l.count(25))
+# print(list.count("apple"))
+
+# print(l.index(25))
+    
+    
+    
