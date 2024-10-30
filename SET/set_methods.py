9cefbf607c4b0b6ce87d27172b696437f74596eb
vinayakck2002 <vinayakck317@gmail.com>
2024-10-30 13:22:44 +0530

    set

---
+++ SET/set_methods.py
@@ -0,0 +1,31 @@
+#SET
+
+#ADD ELEMENT IN SET LIST
+s={1,2,3,4}
+s.add(5)
+print(s)
+
+#ADDING MORE THAN ONE ELEMENTS IN SET LIST
+s.update({6,7})
+print(s)
+
+#DELETE ELEMENTS IN THE SET LIST
+s={1,2,5,8,9}
+s.pop(2)
+print(s)
+
+s.clear()
+print(s)
+
+#DELETE ELEMENTS IN THE SET LIST 
+s={5,6,7,9,8,0}
+s.remove(8)#we can see the error in remove
+print(s)
+
+s={3,6,7,8}
+s.discard(5)#we can't see any error in discard
+print(s)
+
+
+
+
--- TUPLE/work22.py
+++ TUPLE/work22.py
@@ -1,15 +1,15 @@
 #TUPLE
 
-#CHANGE ONE VARIABLE ADD IN TUPLE LIST
-t=(1,2,3,4)
-l=list(t)
-l.append(5)
-t=tuple(l)
-print(t)
+# ADD ONE VARIABLE  IN TUPLE LIST
+# t=(1,2,3,4)
+# l=list(t)
+# l.append(5)
+# t=tuple(l)
+# print(t)                                                               
 
-#CHANGE MORE THAN VARIABLE ADD IN TUPLE LIST
+#ADD MULTIPLE VARIABLE IN TUPLE LIST
 t=(1,2,3,4)
 l=list(t)
-l.extend(5,6,7)
+l.extend([5,6,7])
 t=tuple(l)
-print(t)
+print(t)  
