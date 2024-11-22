2dfa7e3a0fec5a6875a813111fc9206e390db31c
vinayakck2002 <vinayakck317@gmail.com>
2024-11-23 00:13:18 +0530

    file handling

---
+++ FILE HANDLING/mul1_5.py
@@ -0,0 +1,7 @@
+n=int(input("Enter a number: "))
+f=open("samples/FILE HANDLING/mul1_5.txt","w")
+for i in range (1,11):
+    for j in range (1,n+1):
+        print('{:<2} x {:<2} = {:<2}'.format(i,j,i*j), end="\t")
+        f.write(str(i) + "x" + str(j) + "=" + str(i*j) + "\t")
+    print()
