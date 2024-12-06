b68920562212e4148dc7798b5af8b97d20de1d46
vinayakck2002 <vinayakck317@gmail.com>
2024-12-06 13:43:55 +0530

    inheritance

--- MODULES/main.py
+++
@@ -1,3 +0,0 @@
-from sample import *
-func()
-func1()
---
+++ OOPS/inheritance.py
@@ -0,0 +1,86 @@
+# #single inheritance
+# class vehicle:
+#     def move():
+#         print("Hello world")
+# class car(vehicle):#inheritance
+#     def four_wheeler():
+#         print("it moves")
+# obj=vehicle
+# obj.move()
+
+# obj1=car
+# obj1.four_wheeler()
+# obj1.move()
+
+
+#---------------------------------------------------------------------------------#
+
+
+# #multiple inheritance
+# class Vehicle:
+#     def move():
+#         print("welcome")
+# class Car:
+#     def four_wheeler():
+#         print("hai")
+# class Bike(Vehicle,Car):
+#     def two_wheeler():
+#         print("of course")
+
+# obj=Bike
+# obj.move()
+# obj.four_wheeler()
+# obj.two_wheeler()
+
+
+#--------------------------------------------------------------------------------#
+
+
+
+# #heirarchial inheritance
+# class Parant():
+#     def vehicle():
+#         print("i am ur dad")
+# class Child_1(Parant):
+#     def car():
+#         print("I am ur first child")
+# class Child_2(Parant):
+#     def bike():
+#         print("I am ur sec child")
+
+# obj=Child_1
+# obj.car()
+# obj.vehicle()
+
+# obj1=Child_2
+# obj1.bike()
+# obj1.vehicle()
+
+
+#-------------------------------------------------------------------#
+
+# #hybrid inheritance
+# class Parant:
+#     def move():
+#         print("I have money")
+# class Child_1(Parant):
+#     def choco():
+#         print("i want 2rs")
+# class Child_2(Parant):
+#     def juice():
+#         print("i want 50rs")
+# class child_3(Child_1,Child_2):
+#     def book():
+#         print("i want 35rs")
+         
+
+# obj=child_3
+# obj.move()
+# obj.book()
+# obj.choco()
+# obj.juice()
+
+
+
+
+
